3db9b932ea8c2f62525c8e317770a19ba0c58b2d
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 23:31:29 -0500

    Clase 3

--- src/main/java/udemy/poo/inicio/CicloSwitch.java
+++ src/main/java/udemy/poo/inicio/CicloSwitch.java
@@ -1,4 +1,24 @@
 package udemy.poo.inicio;
 
+import javax.swing.*;
+
+/*
+  Programa que reciba por grafico un número y este número sea mayor a 1 y menor a 7 el cual de un resultado de la semana
+ */
 public class CicloSwitch {
+  public static void main(String[] args) {
+    String dato = JOptionPane.showInputDialog(null, "Dame un día de la semana: ");
+    String dia = "";
+    int numUno = Integer.parseInt(dato);
+    switch (numUno) {
+      case 1: dia = "El día es Lunes";
+      break;
+      case 2: dia = "El día es Martes";
+      break;
+      case 3: dia = "El día es Miércoles";
+      break;
+      case 4: dia = "El día es Jueves";
+      break;
+    }
+  }
 }
