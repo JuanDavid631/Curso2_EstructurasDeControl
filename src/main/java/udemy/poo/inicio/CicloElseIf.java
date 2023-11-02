d1d5f87022b9c9b50569642d4e41777d20f7944f
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 23:14:45 -0500

    Clase 1

--- src/main/java/udemy/poo/inicio/CicloElseIf.java
+++ src/main/java/udemy/poo/inicio/CicloElseIf.java
@@ -3,12 +3,18 @@ package udemy.poo.inicio;
 import javax.swing.*;
 
 /*
-    Recibir numero por graficos, evaluar si el numero es par o impar
+    Recibir numero por graficos, evaluar si el numero es par o impar, mostrar el resultado en grafico.
  */
 public class CicloElseIf {
   public static void main(String[] args) {
     String dato = JOptionPane.showInputDialog(null, "Ingrese un número positivo: ");
-
-
+    String par = "";
+    int numUno = Integer.parseInt(dato);
+    if (numUno % 2 == 0) {
+      par = par + "El numero " + numUno + " es par";
+    } else {
+      par = par + "El numero " + numUno + " es impar";
+    }
+    JOptionPane.showMessageDialog(null, par, "¿Qué número es?", JOptionPane.INFORMATION_MESSAGE);
   }
 }
