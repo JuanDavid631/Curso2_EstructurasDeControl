3ca9680dfaa529ae5b3c23b7c46c721c1313033e
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 23:18:18 -0500

    Clase 2

---
+++ src/main/java/udemy/poo/inicio/CicloElseIfDos.java
@@ -0,0 +1,7 @@
+package udemy.poo.inicio;
+/*
+    Recibir numero por graficos, evaluar si el numero es igual o no a ciertos criterios que se proporcionan.
+ */
+public class CicloElseIfDos {
+    
+}
