e49a71b67df27e8555cd06a92501cf59caae72b6
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 23:55:20 -0500

    Clase 6

---
+++ src/main/java/udemy/poo/inicio/CicloWhile.java
@@ -0,0 +1,11 @@
+package udemy.poo.inicio;
+
+import javax.swing.*;
+/*
+    Tablas de multiplicar del 1 al 10, el numero lo proporciona el usuario
+ */
+public class CicloWhile {
+  public static void main(String[] args) {
+    String dato = JOptionPane.showInputDialog(null, )
+  }
+}
