c064fa7b110219f89e11a0cd4406f61cb6eed526
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 23:53:40 -0500

    Clase 5

--- src/main/java/udemy/poo/inicio/CicloFor.java
+++ src/main/java/udemy/poo/inicio/CicloFor.java
@@ -1,9 +1,19 @@
 package udemy.poo.inicio;
+
+import javax.swing.*;
+
 /*
     Tablas de multiplicar del 1 al 10, el numero lo proporciona el usuario
  */
 public class CicloFor {
   public static void main(String[] args) {
+    String dato = JOptionPane.showInputDialog(null, "Ingrese un número para mostrar la tabla de multiplicar: "), cadena = "";
+    float datoNum = Float.parseFloat(dato);
+    int ayuda = 0;
 
+    for (ayuda = 1; ayuda <= 10; ayuda++) {
+      cadena = cadena + datoNum + " x " + ayuda + " = " + (datoNum * ayuda) + "\n";
+    }
+    JOptionPane.showMessageDialog(null, cadena);
   }
 }
