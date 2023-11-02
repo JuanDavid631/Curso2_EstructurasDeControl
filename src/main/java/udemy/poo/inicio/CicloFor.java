c0f78f55426d99758b57e90473f4a4dc98882b91
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 23:48:54 -0500

    Clase 5

---
+++ src/main/java/udemy/poo/inicio/CicloFor.java
@@ -0,0 +1,9 @@
+package udemy.poo.inicio;
+/*
+    Tablas de multiplicar del 1 al 10, el numero lo proporciona el usuario
+ */
+public class CicloFor {
+  public static void main(String[] args) {
+
+  }
+}
