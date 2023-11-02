ace1e71d3cf31b47f7624adf3620c9303d1e3670
JuanMontoya <mjuandavid560@gmail.com>
2023-11-02 00:01:30 -0500

    Clase 7

---
+++ src/main/java/udemy/poo/inicio/CicloDoWhile.java
@@ -0,0 +1,12 @@
+package udemy.poo.inicio;
+
+import javax.swing.*;
+/*
+    Tablas de multiplicar del 1 al 10, el numero lo proporciona el usuario
+ */
+public class CicloDoWhile {
+  public static void main(String[] args) {
+    String dato = JOptionPane.showInputDialog(null, "Ingrese un numero: ");
+    int numUno = Integer.parseInt(dato);
+  }
+}
