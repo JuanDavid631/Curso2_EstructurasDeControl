454cb47884a1f46847b2e5eb0343434a858d9c08
JuanMontoya <mjuandavid560@gmail.com>
2023-11-01 23:23:45 -0500

    Clase 2

--- src/main/java/udemy/poo/inicio/CicloElseIfDos.java
+++ src/main/java/udemy/poo/inicio/CicloElseIfDos.java
@@ -1,7 +1,23 @@
 package udemy.poo.inicio;
+
+import javax.swing.*;
 /*
     Recibir numero por graficos, evaluar si el numero es igual o no a ciertos criterios que se proporcionan.
  */
 public class CicloElseIfDos {
-    
+  public static void main(String[] args) {
+    String dato = JOptionPane.showInputDialog(null, "Dame un número");
+    int numUno = Integer.parseInt(dato);
+    String igual = "";
+    if (numUno == 10){
+      igual = igual + " El número es igual a 10";
+    } else if (numUno == 20) {
+      igual = igual + " El número es igual a 20";
+    } else if (numUno == 30) {
+      igual = igual + " El número es igual a 30";
+    } else {
+      igual = igual + " El número no está dentro del rango permitido";
+    }
+    JOptionPane.showMessageDialog(null, igual);
+  }
 }
